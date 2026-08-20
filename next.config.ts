2c00befede4d304b75316b64203523922b5888fe
Tharun Sebastian <tharun.sebastian@siemens.com>
2026-08-20 12:14:24 +0530

    Configure basePath for GitHub Pages hosting

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export", // Required for GitHub Pages export
+  basePath: isProd ? "/knowledge-base" : "",
   images: {
     unoptimized: true, // Required, as Next.js image optimization doesn't work for static export
   },
-  experimental: {
-    // Allows markdown files to be read dynamically during export
-    // If you have any errors building, you can remove this or modify as needed.
-  }
 };
 
 export default nextConfig;
+
